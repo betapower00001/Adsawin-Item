b2701084093333962673d033e91ed0f28ffb3123
Subin-mungmee <betapower00002@gmail.com>
2025-10-06 01:18:05 +0700

    2

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -29,7 +29,7 @@ export const categories = [
     patterns: [
       {
         id: "dream",
-        name: "ลายดรีมแฟนตาซี--1111111111",
+        name: "ลายดรีมแฟนตาซี--11555511111111",
         img: "/112521.jpg",
         products: [
           { name: "Dimoo Dream", img: "/112521.jpg" },
