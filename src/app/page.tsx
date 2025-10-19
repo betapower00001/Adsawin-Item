1459c799f78b3515627cc18c2fe0061bd5a0fb6b
Subin-mungmee <betapower00002@gmail.com>
2025-10-19 10:39:35 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import CharacterGrid from "../Component/CharacterGrid";
 import { Container } from "react-bootstrap";
 
 const characters = [
-  { name: "ปลั๊กไฟตามสั่งB2B", img: "/MENU-ICON.png" },
-  { name: "CATALOG", img: "/MENU-ICON-CATALOG.png" },
-  { name: "SEASONAL", img: "/characters/skullpanda.png" },
-  { name: "MUTELU", img: "/characters/crybaby.png" },
-  { name: "FASHION&TREND", img: "/characters/hirono.png" },
-  { name: "FUN&FANTASY", img: "/characters/twinkle.png" },
-  { name: "AUTHENTIC THAI", img: "/characters/hacipupu.png" },
-  { name: "OTAKU", img: "/characters/pucky.png" },
-  { name: "FOOD", img: "/characters/zsiga.png" },
+  { name: "ปลั๊กไฟตามสั่งB2B", img: "/MENU-ICON-CATALOG.png" },
+  { name: "CATALOG", img: "/MENU-ICON-CATALOG.jpg" },
+  { name: "SEASONAL", img: "/MENU-ICON-SEASONAL.png" },
+  { name: "MUTELU", img: "/MENU-ICON.png" },
+  { name: "FASHION&TREND", img: "/MENU-ICON-Fashion.png" },
+  { name: "FUN&FANTASY", img: "/MENU-ICON-Fashion.png" },
+  { name: "AUTHENTIC THAI", img: "/MENU-ICON-Thai.png" },
+  { name: "OTAKU", img: "/MENU-ICON-Otaku.png" },
+  { name: "FOOD", img: "/MENU-ICON-Food.png" },
   { name: "PETS", img: "/characters/dimoo.png" },
 ];
 
