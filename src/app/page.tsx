b34bd503567500383b13efd9b5ce54f57bba4371
Subin-mungmee <betapower00002@gmail.com>
2025-10-24 22:41:28 +0700

    Up vedeo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,47 @@ import CharacterGrid from "../Component/CharacterGrid";
 import { Container } from "react-bootstrap";
 
 const characters = [
+
+  {
+    name: "SEASONAL",
+    img: "/MENU-ICON-SEASONAL.png",
+    video: "/MENU-ICON-SEASONAL.mp4",
+  },
+  {
+    name: "FUN&FANTASY",
+    img: "/MENU-ICON-Fantasy.png",
+    video: "/Menu-fan.mp4",
+  },
+  {
+    name: "OTAKU",
+    img: "/MENU-ICON-Otaku.png",
+    video: "/MENU-ICON-Otaku.mp4",
+  },
+  {
+    name: "MUTELU",
+    img: "/MENU-ICON.jpg",
+    video: "/MENU-ICON-MU.mp4",
+  },
+  {
+    name: "AUTHENTIC THAI",
+    img: "/MENU-ICON-Thai.png",
+    video: "/MENU-ICON-Thai.mp4",
+  },
+  {
+    name: "FOOD",
+    img: "/MENU-ICON-Food.png",
+    video: "/MENU-ICON-Food.mp4",
+  },
+  {
+    name: "FASHION&TREND",
+    img: "/MENU-ICON-Fashion.png",
+    video: "/MENU-ICON-FASHION-TREND.mp4",
+  },
+  {
+    name: "PETS",
+    img: "/MENU-ICON-PET.jpg",
+    video: "/MENU-ICON-PET.mp4",
+  },
   {
     name: "ปลั๊กไฟตามสั่งB2B",
     img: "/MENU-ICON-CATALOG.png",
@@ -12,46 +53,6 @@ const characters = [
     name: "CATALOG",
     img: "/MENU-ICON-CATALOG.jpg",
     video: "/videos/catalog.mp4",
-  },
-  {
-    name: "SEASONAL",
-    img: "/MENU-ICON-SEASONAL.png",
-    video: "/MenuMp4.mp4",
-  },
-  {
-    name: "MUTELU",
-    img: "/MENU-ICON.jpg",
-    video: "/MenuMp4.mp4",
-  },
-  {
-    name: "FASHION&TREND",
-    img: "/MENU-ICON-FASHION-TREND.jpg",
-    video: "/videos/fashion.mp4",
-  },
-  {
-    name: "FUN&FANTASY",
-    img: "/MENU-ICON-Fashion.png",
-    video: "/Menu-fan.mp4",
-  },
-  {
-    name: "AUTHENTIC THAI",
-    img: "/MENU-ICON-Thai.png",
-    video: "/videos/thai.mp4",
-  },
-  {
-    name: "OTAKU",
-    img: "/MENU-ICON-Otaku.png",
-    video: "/videos/otaku.mp4",
-  },
-  {
-    name: "FOOD",
-    img: "/MENU-ICON-Food.png",
-    video: "/videos/food.mp4",
-  },
-  {
-    name: "PETS",
-    img: "/MENU-ICON-PET.jpg",
-    video: "/Menu-Pet.mp4",
   },
 ];
 
